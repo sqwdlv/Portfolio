2b123865e4c7e3e1fe2ebe8698de1fd1b12fb502
Dagnis Abols <dagnisinfo@gmail.com>
2023-09-03 18:04:44 +0300

    Edit map.js / marker location

--- Portfolio/script/map.js
+++ Portfolio/script/map.js
@@ -3,17 +3,17 @@ let map;
 
 async function initMap() {
 
-  // The location of Uluru
+
 
   const position = { lat: 56.946285, lng: 24.105078 };
 
-  // Request needed libraries.
 
-  //@ts-ignore
+
+
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+
 
   map = new Map(document.getElementById("map"), {
     zoom: 4,
@@ -21,7 +21,7 @@ async function initMap() {
     mapId: "Riga",
   });
 
-  // The marker, positioned at Uluru
+
 
   const marker = new AdvancedMarkerElement({
     map: map,
